a2afba4d087df94c012eaceb906750f0f9e1a6f2
Umer Sheryar <sheryarbaloch67@gmail.com>
2023-01-07 15:40:41 +0500

    Added signup and signin pages in django

--- Back-end/main/urls.py
+++ Back-end/main/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.cover_page)
+    path('', views.cover),
+    path('signup', views.signup),
+    path('signin', views.signin),
 ]
--- Back-end/main/views.py
+++ Back-end/main/views.py
@@ -3,5 +3,11 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def cover_page(request):
+def cover(request):
     return render(request, 'index.html')
+
+def signup(request):
+    return render(request, 'signup.html')
+
+def signin(request):
+    return render(request, 'signin.html')
